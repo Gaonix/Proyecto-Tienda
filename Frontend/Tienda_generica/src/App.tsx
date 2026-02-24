1b00f73d1289f6e06f96b76e7e388ed54a37d62d
Fernando Gaona <fernandocpagna@gmail.com>
2026-02-23 21:10:20 -0500

    Ag

---
+++ Frontend/Tienda_generica/src/App.tsx
@@ -0,0 +1,12 @@
+import {login}
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/Tienda_generica/src/login.tsx
@@ -0,0 +1,10 @@
+function Login(){
+(<>
+<form action="">
+<label htmlFor="nombreUsuario">Usuario:</label><input type="text" id="in-nombreUsuario"/>
+<label htmlFor="contraseña">Contraseña</label><input type="text" id="in-contraseña"/>
+<button>ingresar</button>
+</form>
+</>)
+}
+export default Login
